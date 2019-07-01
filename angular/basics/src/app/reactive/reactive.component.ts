ad93841e0cb6796555ccd706bde6ebcac6d14458
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-07-01 08:42:25 +0530

    reactive forms

--- angular/basics/src/app/reactive/reactive.component.ts
+++ angular/basics/src/app/reactive/reactive.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
+function ratingFn(e:AbstractControl){
+  var range = e.value;
+  if(range < 0 && range > 10){
+   return  {
+      'watch':true
+    }
+  }
+  return false;
+}
+
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
+
 export class ReactiveComponent implements OnInit {
 
   constructor() { }
@@ -13,8 +25,10 @@ export class ReactiveComponent implements OnInit {
   ngOnInit() {
      this.customerInfo = new FormGroup({
        firstName: new FormControl(""),
-       phone: new FormControl(""),
-       emailId: new FormControl("",[Validators.required,Validators.email])
+       phone: new FormControl("",[Validators.required,Validators.maxLength(10),Validators.minLength(10)]),
+       emailId: new FormControl("",[Validators.required,Validators.email]),
+      //  rating:new FormControl("",[Validators.required,Validators.max(10),Validators.min(1)])
+      rating:new FormControl("",[Validators.required, ratingFn])
      });
   }
   testMthd(){
