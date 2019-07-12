8dc0ae3ec0d1bdae1c6e901631df52e8cab7798c
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-07-12 08:47:35 +0530

    ajax

--- angular/routingsandmodules/src/app/approutingmodule.ts
+++ angular/routingsandmodules/src/app/approutingmodule.ts
@@ -1,13 +1,14 @@
 import { CustomersComponent } from "./customers/customers.component";
 import { OrdersComponent } from "./orders/orders.component";
 import { NgModule } from "@angular/core";
-
+import { RouterModule } from "@angular/router";
 const route = [
     {path:"customers", component:CustomersComponent},
     {path:"orders", component:OrdersComponent},
 ]
 @NgModule({
-    
+    imports:[RouterModule.forRoot(route)],
+    exports:[RouterModule]
 })
 
 
