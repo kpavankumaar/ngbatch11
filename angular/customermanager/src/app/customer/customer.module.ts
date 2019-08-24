598d5076249a89fe986adc8a85d5a1050779ef18
unknown <k.pavankumaar@gmail.com>
2019-08-24 14:38:50 +0530

    editcustomer page

--- angular/customermanager/src/app/customer/customer.module.ts
+++ angular/customermanager/src/app/customer/customer.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerDetailComponent } from './customer-detail.component';
 import { CustomerOrdersComponent } from './customer-orders.component';
@@ -10,6 +10,7 @@ import { CustomerComponent } from './customer.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     CustomerRoutingModule
   ],
   declarations: [CustomerDetailComponent, CustomerOrdersComponent, EditCustomerComponent, CustomerComponent]
--- angular/customermanager/src/app/customer/edit-customer.component.ts
+++ angular/customermanager/src/app/customer/edit-customer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../core/services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-customer',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCustomerComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dataService:DataService, private route:ActivatedRoute) { }
+  editCustDetails;
   ngOnInit() {
+    this.route.parent.params.subscribe((data)=>{
+      console.log('param observable data',data);
+      this.dataService.getCustomerDataById(data.id).subscribe((data)=>{
+        console.log(data)
+        this.editCustDetails = data;
+      })
+    });
+    
   }
 
+  submitDetails(formGroup){
+    
+  }
 }
